74bedff067192d34f065caf0cfb73a4127e3f45a
ENRAG3DCHICKEN <yiu.jackie@gmail.com>
2021-12-06 14:13:32 -0500

    Working Implementation of Firestore Add/Delete

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const { initializeApp } = require('firebase-admin/app');
 
-var admin = require("firebase-admin");
-var serviceAccount = require("./cryptoloans-82f2d-firebase-adminsdk-rmitf-22c7ceea6f.json");
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
+const serviceAccount = require("./cryptoloans-82f2d-firebase-adminsdk-rmitf-22c7ceea6f.json");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  
   .get('/', (req, res) => {
     console.log("Hello World!")
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
+
+      initializeApp({
+      credential: cert(serviceAccount)
     });
+
+    const db = getFirestore();
+
     res.render('pages/index')
   })
 
