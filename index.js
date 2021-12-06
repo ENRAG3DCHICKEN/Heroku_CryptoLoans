ff40b608b21032453175487a26caf675062a388a
ENRAG3DCHICKEN <yiu.jackie@gmail.com>
2021-12-06 12:40:38 -0500

    Added and Initialized Firebase Admin SDK

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { initializeApp } = require('firebase-admin/app');
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./cryptoloans-82f2d-firebase-adminsdk-rmitf-22c7ceea6f.json");
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  
+  .get('/', (req, res) => {
+    console.log("Hello World!")
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    });
+    res.render('pages/index')
+  })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
