c5a24c43ab6d3298c08d24f636c8bcd522d8103b
ENRAG3DCHICKEN <yiu.jackie@gmail.com>
2021-12-11 00:28:27 -0500

    Quote from CMC works and prints to console. Postman Code used.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const twilio = require('twilio');
 
 
 
+const axios = require("axios");
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -22,5 +25,32 @@ express()
     });
     const db = getFirestore();
     res.render('pages/index')
+  })  
+  .get('/test', (req, res) => {
+    console.log("Testing 123!")
+    axios.get("https://reqres.in/api/users")
+      .then(response => 
+        res.json(response.data)
+      )
+  })
+  .get('/quotefromcmc', (req, res) => {
+    
+    console.log("Testing CMC Route!")
+
+    var request = require('request');
+    var options = {
+      'method': 'GET',
+      'url': 'http://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=eth,btc',
+      'headers': {
+        'X-CMC_PRO_API_KEY': 'a3647fe0-fec6-47b5-8e13-e1758053cb89'
+      }
+    };
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+    });
+    
+    
+    console.log("end")
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
